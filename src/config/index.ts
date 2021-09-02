162af313f0069e0ed27f05f60c90e593d861fb91
Calin Tamas <calintamas2@gmail.com>
2021-09-02 16:15:37 +0300

    feat: load config via env file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,13 @@
+import env from 'react-native-config';
+
 const DEFAULT_LANGUAGE = 'en';
 const DEFAULT_THEME = 'base';
 
 const REQUEST_TIMEOUT = 10 * 1000; // ms
 
 const GITHUB_HOST = 'https://api.github.com';
-const GITHUB_TOKEN = '';
+
+const { GITHUB_TOKEN } = env;
 
 export {
   DEFAULT_THEME,
